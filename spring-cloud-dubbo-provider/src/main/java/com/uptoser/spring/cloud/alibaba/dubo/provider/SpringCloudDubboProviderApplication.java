a17d691866efdf3947aad30245286fc496cc3f5a
uptoser <450052550@qq.com>
2025-01-25 20:28:02 +0800

    cloud dubbo nacos

--- spring-cloud-dubbo-provider/src/main/java/com/uptoser/spring/cloud/alibaba/dubo/provider/SpringCloudDubboProviderApplication.java
+++ spring-cloud-dubbo-provider/src/main/java/com/uptoser/spring/cloud/alibaba/dubo/provider/SpringCloudDubboProviderApplication.java
@@ -4,6 +4,11 @@ import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Nacos 2.2.10-RC1版本新增了gRPC通信方式，需要额外开放9848和9849端口。
+ * 如果只映射了8848端口，可能会导致连接失败。
+ * 使用Docker时需要映射9848和9849端口‌
+ */
 @DubboComponentScan//Dubbo的扫描注解
 @SpringBootApplication
 public class SpringCloudDubboProviderApplication {
